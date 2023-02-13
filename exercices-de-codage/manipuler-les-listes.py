86e2f0cf3da62fc762bb18c24bdefc9681215aa6
Flanker213 <assas-a@outlook.fr>
2023-02-13 09:29:31 +0100

    Exercice complété

--- exercices-de-codage/manipuler-les-listes.py
+++ exercices-de-codage/manipuler-les-listes.py
@@ -29,3 +29,17 @@ langage =
 # Changez la position de l'élément 'Python' dans la liste pour qu'il se retrouve à la fin de la liste (["Java", "C++", "Python"])
 liste = ["Java", "Python", "C++"]
 """
+
+# Récupérez le premier et le dernier nombre contenus dans cette liste dans les variables 'nombre_premier' et 'nombre_dernier'.
+nombres = [1, 2, 3, 4, 5, 4, 3, 2, 1]
+nombre_premier = nombres[0] # Je récupère le premier élément de la liste avec l'index 0 qui représente le premier élément de la liste
+nombre_dernier = nombres[-1] # Je récupère le dernier élément de la liste avec l'index -1 qui représente le dernier élément nombre
+
+# Récupérer l'élément 'Python' contenu dans la liste dans la variable 'langage'.
+langages = ["Java", "Python", "C++"]
+langage = langages[1] # Comment "Python" est le deuxième élément dans la liste, j'utilise l'index 1 pour le récupérer
+
+# Changez la position de l'élément 'Python' dans la liste pour qu'il se retrouve à la fin de la liste (["Java", "C++", "Python"])
+liste = ["Java", "Python", "C++"]
+liste.remove("Python") # J'utilise la methode remove() pour supprimer un élément spécifique de la liste de sa position actuelle
+liste.append("Python") # Et j'utilise la methode append() pour l'ajouter à la fin de la liste
